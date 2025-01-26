6d277fa3351f0ac68cbb7e7aa0ae2de15ee31fe0
TanmayB <tanmay.bagwe.tb@gmail.com>
2025-01-26 17:12:31 +0900

    Added better-sqlite3

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -10,10 +10,10 @@ export default function App(): JSX.Element {
           <img src={logo} alt="Album" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">New album is released!</h2>
-          <p>Click the button to listen on Spotiwhy app.</p>
+          <h2 className="card-title">Let&apos;s get you learning Japanese!</h2>
+          <p>Click the button to get started</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Listen</button>
+            <button className="btn btn-primary">Get Started</button>
           </div>
         </div>
       </div>
