7c195c091b73222c77a57b57cc0ac38eaf105522
TanmayB <110551323+happygoluckycodeeditor@users.noreply.github.com>
2025-03-16 09:46:18 +0900

    Added placeholder text

--- src/renderer/src/components/About.tsx
+++ src/renderer/src/components/About.tsx
@@ -9,6 +9,22 @@ const Options: React.FC = () => {
         Here comes the About Page. This page includes all of the metadata and history about the
         Application
       </p>
+      <p className="mt-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies
+        tincidunt, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Donec euismod, nisl
+        eget ultricies tincidunt, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl.
+        Donec euismod, nisl eget ultricies tincidunt, nunc nisl aliquam nisl, eget aliquam nisl nisl
+        sit amet nisl. Donec euismod, nisl eget ultricies tincidunt, nunc nisl aliquam nisl, eget
+        aliquam nisl nisl sit amet nisl. Donec euismod, nisl eget ultricies tincidunt, nunc nisl
+        aliquam nisl, eget aliquam nisl nisl sit amet nisl. Vestibulum ante ipsum primis in faucibus
+        orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis porttitor congue, elit
+        erat euismod orci, ac placerat dolor lectus quis orci. Phasellus consectetuer vestibulum
+        elit. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Vestibulum fringilla
+        pede sit amet augue. In turpis. Pellentesque posuere. Praesent turpis. Aenean posuere,
+        tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis
+        lacus. If you need to generate lorem ipsum text programmatically, you can use a command-line
+        tool like:
+      </p>
 
       {/* We'll add more content here later */}
     </div>
