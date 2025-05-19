85d6162dc0ec9f94c54b24705e274f9d65af4f1d
TanmayB <tanmay.bagwe.tb@gmail.com>
2025-05-19 23:17:29 +0900

    Added lesson Page

---
+++ src/renderer/src/components/LessonPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Options: React.FC = () => {
+  return (
+    <div className="w-screen max-w-full p-10 pt-20 sm:pl-10 sm:pr-10 md:pl-24 md:pr-24 bg-gray-200">
+      {/* Title */}
+      <h1 className="text-4xl font-bold mb-4">Lesson Page</h1>
+      <p className="mt-4">
+        This page will have the interface to display the selected Lesson&apos;s Lesson Content and
+        content.
+      </p>
+
+      {/* We'll add more content here later */}
+    </div>
+  )
+}
+
+export default Options
