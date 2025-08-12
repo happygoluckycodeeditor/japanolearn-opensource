373e8a006897d237e7d065511fd6cb1f5a9d67a5
TanmayB <tanmay.bagwe.tb@gmail.com>
2025-08-12 23:31:50 +0900

    fix: fixed and changed the kana mode

--- resources/Hiragana Katakana Learning Interface/App.tsx
+++
@@ -1,9 +0,0 @@
-import { JapaneseLearning } from './components/JapaneseLearning';
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-background p-4 flex items-center justify-center">
-      <JapaneseLearning />
-    </div>
-  );
-}
