0595f25b68ca42b43168707626c3b76ce602fa6d
TanmayB <tanmay.bagwe.tb@gmail.com>
2026-05-03 16:46:48 +0900

    chore: upgrade to react 19 and refactoring code

--- src/renderer/src/components/options/data/LessonEditor.tsx
+++ src/renderer/src/components/options/data/LessonEditor.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import ReactQuill from 'react-quill'
-import 'react-quill/dist/quill.snow.css' // This is all you need!
+import ReactQuill from 'react-quill-new'
+import 'react-quill-new/dist/quill.snow.css'
 import { Lesson, LessonQuestion, Exercise } from '../../../types/database'
 import LessonQuestionManager from './LessonQuestionManager'
 
